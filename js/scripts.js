2d2217a509e68bbdec6e439aa7c52a07ae7254c2
Raghul Mukundan <raghulmuk@gmail.com>
2024-06-25 23:17:53 -0500

    add scala section and make it little bit functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
     // Load content when navigation links are clicked
-    $('#navJava').on('click', function(e) {
+    $('#mainNav').on('click', '#navJava', function(e) {
         e.preventDefault();
+        $('#navJava').addClass('active');
+        $('#navScala').removeClass('active');
         $('#indexList').load('java/index.html');
         $('#contentArea').html('<h2>Java</h2><p>Select a subsection from the left to view its content here.</p>');
     });
 
-    $('#navScala').on('click', function(e) {
+    $('#mainNav').on('click', '#navScala', function(e) {
         e.preventDefault();
+        $('#navScala').addClass('active');
+        $('#navJava').removeClass('active');
         $('#indexList').load('scala/index.html');
         $('#contentArea').html('<h2>Scala</h2><p>Select a subsection from the left to view its content here.</p>');
     });
